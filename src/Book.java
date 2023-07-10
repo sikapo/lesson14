5c757bfc1001a4ea2d3cc3e7f0d6612a44015e62
Owner <vitycity79@gmail.com>
2023-07-11 01:50:21 +0300

    done!

--- src/Book.java
+++ src/Book.java
@@ -1,23 +1,72 @@
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Book {
     private final String bookName;
+    private final String file;
 
     public Book(String bookName) {
         this.bookName = bookName;
+        String filePath = "D:/IT Hillel/JavaPro/lesson14/src";
+        this.file = filePath + "/" + bookName;
     }
 
     public String getBookName() {
         return bookName;
     }
+    public String getFile() {
+        return file;
+    }
 
-//    public int moreThanTwo (String bookName){
-//        return;
-//    }
-//    public int uniqueWords (FileReader bookName) {
-//        int uniqueWordsCounter = 0;
-//        bookName.
-//
-//        return;
-//    }
+    public void mostPopular (FileReader bookName) throws IOException {
+        ArrayList<String> words = new ArrayList<>();
+        HashMap<String, Integer> mostPopular = new HashMap<>();
+        String text;
+        int c;
+        StringBuilder wordBuilder = new StringBuilder();
+        while ((c = bookName.read()) != -1) {
+            wordBuilder.append((char) c);
+        }
+        text = wordBuilder.toString();
+        Pattern pattern = Pattern.compile("\\b\\p{L}+\\b");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            if (matcher.group().toLowerCase().length() > 2) {
+            words.add(matcher.group().toLowerCase());
+            }
+        }
+        for (String word : words) {
+            if (!mostPopular.isEmpty()) {
+                if (mostPopular.containsKey(word)) {
+                    mostPopular.replace(word, mostPopular.get(word), mostPopular.get(word) + 1);
+                }
+                else mostPopular.put(word, 1);
+            }
+            else mostPopular.put(word, 1);
+        }
+        HashMap<String, Integer> mostPopularTen = new HashMap<>();
+        mostPopular.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(w -> mostPopularTen.put(w.getKey(), w.getValue()));
+        System.out.println("Here is top 10 most used more than two symbols words: \n" + mostPopularTen);
+    }
+    public void uniqueWords (FileReader bookName) throws IOException {
+        HashSet <String> unique = new HashSet<>();
+        int c;
+        StringBuilder wordBuilder = new StringBuilder();
+        while ((c = bookName.read()) != -1) {
+                wordBuilder.append((char) c);
+        }
+        String text = wordBuilder.toString();
+        Pattern pattern = Pattern.compile("\\b\\p{L}+\\b");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            unique.add(matcher.group().toLowerCase());
+        }
+        System.out.println("Your book " + "\"" + getBookName() + "\"" + " has " + unique.size() + " unique words.");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input the book title");
         Book book = new Book(scanner.nextLine());
 
-        try (FileReader fileReader = new FileReader(book.getBookName())){
-            System.out.println(fileReader);
+        try (FileReader fileReader = new FileReader(book.getFile())){
+            book.mostPopular(fileReader);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("No such book was found. Please, enter the correct title.");
+        }
+
+        try (FileReader fileReader = new FileReader(book.getFile())){
+            book.uniqueWords(fileReader);
+        } catch (IOException e) {
+            System.err.println("No such book was found. Please, enter the correct title.");
         }
     }
 }
