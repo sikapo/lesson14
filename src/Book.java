e850818bd1385328a2e96cf8dd87adb28d87c9ab
Owner <vitycity79@gmail.com>
2023-07-10 18:34:56 +0300

    commit 2

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+import java.io.FileReader;
+
+public class Book {
+    private final String bookName;
+
+    public Book(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+//    public int moreThanTwo (String bookName){
+//        return;
+//    }
+//    public int uniqueWords (FileReader bookName) {
+//        int uniqueWordsCounter = 0;
+//        bookName.
+//
+//        return;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the book title");
+        Book book = new Book(scanner.nextLine());
+
+        try (FileReader fileReader = new FileReader(book.getBookName())){
+            System.out.println(fileReader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
